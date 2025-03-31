d6bb05f432a27b6410448842e3fc62c8e683d875
creativecaleb <caleb.velluppillai@gmail.com>
2025-04-01 10:50:35 +1300

    Added .csv files for holding seat and other data Started creating the GUI layout

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from tkinter import *
 
 class Window():
-    def __init__(self):
-        pass
+    def __init__(self, root):
+        self.root = root
+        # Creates the main 'canvas' or background window for the rest of the GUI elements to go on
+        canvas = Canvas(root, width=1300, height=700, bg='grey')
+        canvas.grid()
+
+        canvas.create_rectangle(300, 0, 301, 700)
 
 class Data():
     def __init__(self):
         pass
     
 def main():
-    pass
+    main_window = Tk()
+    Window(main_window)
+    main_window.title('Circus tickets')
+    main_window.mainloop()
 
 if __name__ == '__main__':
     main()
