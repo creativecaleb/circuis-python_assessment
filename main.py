cb73f6ccab72bf3a106a62346ceff7c6f843da58
creativecaleb <caleb.velluppillai@gmail.com>
2025-04-01 10:04:03 +1300

    Added main classes Added __name__ == main functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from tkinter import *
+
+class Window():
+    def __init__(self):
+        pass
+
+class Data():
+    def __init__(self):
+        pass
+    
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
